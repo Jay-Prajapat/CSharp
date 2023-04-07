25546ceb8f3a065ea2b386838289c27a850e0e64
Jay-Prajapat <jayprajapati.1502@gmail.com>
2023-04-07 12:35:44 +0530

    Practical6 Task- Delegates and Events.
    in this task, implemented ProcessBusinessLogic  class having ProcessCompleted event and startProcess method to invoke OnProcessCompleted.

---
+++ Practical6/Practical6/ProcessBusinessLogic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical6
+{
+    
+    public delegate void Notify();
+    public class ProcessBusinessLogic
+    {
+        public event Notify ProcessCompleted;
+        /// <summary>
+        /// This method will start the process and call the OnProcessCompleted method.
+        /// </summary>
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started");
+            OnProcessCompleted();
+        }
+        /// <summary>
+        /// This will invoke the ProcessCompleted event.
+        /// </summary>
+        protected virtual void OnProcessCompleted()
+        {
+            ProcessCompleted?.Invoke();
+        }
+    }
+}
